99031433d38fdb4d418fca9aa0625c0c51a564a3
jakoblrosen <jake3.rosen@gmail.com>
2023-04-24 14:14:33 -0600

    added logging and relative time function

--- birdbot.py
+++ birdbot.py
@@ -1,5 +1,7 @@
 import os
+import math
 import time
+import logging
 import requests
 import random
 import schedule
@@ -14,11 +16,8 @@ def get_galaxy():
     dotenv_path = Path('db.env')
     load_dotenv(dotenv_path=dotenv_path)
 
-    COUCH_USER = os.getenv('COUCH_USER')
-    COUCH_PASSWORD = os.getenv('COUCH_PASSWORD')
-    COUCH_HOST = os.getenv('COUCH_HOST')
-    COUCH_PORT = os.getenv('COUCH_PORT')
-    couch = couchdb.Server(f'http://{COUCH_USER}:{COUCH_PASSWORD}@{COUCH_HOST}:{COUCH_PORT}')
+    couch = couchdb.Server(f'http://{os.getenv("COUCH_USER")}:{os.getenv("COUCH_PASSWORD")}'
+                           f'@{os.getenv("COUCH_HOST")}:{os.getenv("COUCH_PORT")}')
     db = couch['dailyastrobotdb']
     galaxy_ids = [id for id in db]
     galaxy_id = random.choice(galaxy_ids)
@@ -29,26 +28,27 @@ def get_galaxy():
 
 # calculate time in years given distance in Mly and speed in km/h
 def calculate_time(distance, speed):
-    KM_TO_MLY = 1.0577e-19
-    H_TO_Y = 1.1415e-4
-    delta = distance / (speed * (KM_TO_MLY / H_TO_Y))
+    km_to_mly = 1.0577e-19
+    h_to_y = 1.1415e-4
+    delta = distance / (speed * (km_to_mly / h_to_y))
     return int(delta)
+
+
+# calculate relativistic time given time and percentage of speed of light
+def calculate_relative_time(delta, c_percent):
+    return int(delta * math.sqrt(1 - c_percent ** 2))
 
 
 def job():
     dotenv_path = Path('secrets.env')
     load_dotenv(dotenv_path=dotenv_path)
 
-    API_KEY = os.getenv('API_KEY')
-    API_KEY_SECRET = os.getenv('API_KEY_SECRET')
-    BEARER_TOKEN = os.getenv('BEARER_TOKEN')
-    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-    ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
-    auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    auth = tweepy.OAuthHandler(os.getenv('API_KEY'), os.getenv('API_KEY_SECRET'))
+    auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
     api = tweepy.API(auth)
 
-    CURRENT_SPACE_TRAVEL_SPEED = 6.92e5     # stored as km/h
+    current_space_travel_speed = 6.92e5  # stored as km/h
+    log = logging.getLogger('dailyastrobot')
 
     try:
         galaxy = get_galaxy()
@@ -60,21 +60,22 @@ def job():
         photo = requests.get(image_url)
         open(fname, 'wb').write(photo.content)
 
-        NEWLINE = '\n'
-        time_to_reach = calculate_time(distance, CURRENT_SPACE_TRAVEL_SPEED)
-        message = f'{galaxy_name} ({galaxy_code}){NEWLINE}' \
-                  f'Distance: {distance} megalight-years{NEWLINE}' \
-                  f'{time_to_reach} years to reach at our current space travel speed'
+        time_to_reach = calculate_time(distance, current_space_travel_speed)
+        time_to_reach_relative = calculate_relative_time(time_to_reach, 0.95)
+        message = (f'{galaxy_name} ({galaxy_code})\n'
+                   f'Distance: {distance} megalight-years\n'
+                   f'{time_to_reach} years to reach at our current space travel speed\n'
+                   f'{time_to_reach_relative} years to reach at 95% the speed of light')
 
         media = api.simple_upload(fname)
         api.update_status(status=message, media_ids=[media.media_id])
-        print(message)
-        print(f'chirp @ {time.asctime()}')
+        log.info(f'chirp @ {time.asctime()}')
     except Exception as err:
-        print(f'error @ {time.asctime()} : {err}')
+        log.warning(f'error @ {time.asctime()} : {err}')
 
 
 def main():
+    logging.Logger('dailyastrobot')
     schedule.every().day.at('12:00').do(job)
     while True:
         schedule.run_pending()
